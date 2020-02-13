0184b835068c5bb00af7db63989a8ffe5b4a104a
Mobius5150 <mike@michaelblouin.ca>
2020-02-12 20:56:14 -0800

    feat(test): add tests for path params

---
+++ packages/ts-api-decorators/test/parameter-runtime-tests/path-params.test.ts
@@ -0,0 +1,98 @@
+import { assert } from 'chai';
+import * as path from 'path';
+import 'mocha';
+import { getCompiledProgram } from '../TestUtil';
+import { ApiMethod } from '../../src';
+import { TestManagedApi } from '../TestTransport';
+
+describe('Path Param Runtime', () => {
+    let api: TestManagedApi;
+    
+    async function testApiMethodInput(url: string, paramName: string, tests: Array<[any, number]>) {
+        for (const [testVal, resultStatus] of tests) {
+            const result = await api.invokeApiCall(ApiMethod.GET, url.replace(`:${paramName}`, testVal), {
+                headers: {},
+                queryParams: {},
+                transportParams: {},
+            });
+
+            assert.equal(result.statusCode, resultStatus, `Expected input of '${testVal}' to give status code ${resultStatus} but got ${result.statusCode}`);
+
+            if (resultStatus >= 200 && resultStatus < 300) {
+                assert.equal(result.body, testVal.toString());
+            }
+        }
+    }
+	
+	before(() => {
+		[api] = getCompiledProgram([
+			path.join(__dirname, 'sources/path-params.ts'),
+        ]);
+        
+        api.init();
+	})
+
+	it('should validate number minimums', async () => {
+        await testApiMethodInput('/numberMin/:num', 'num', [
+            [0, 200],
+            [1, 200],
+            [-1, 400],
+            ['a', 400],
+            ['', 404],
+            [true, 400],
+            [false, 400],
+        ]);
+    });
+
+    it('should validate number maximums', async () => {
+        await testApiMethodInput('/numberMax/:num', 'num', [
+            [0, 200],
+            [1, 400],
+            [-1, 200],
+            ['a', 400],
+            [true, 400],
+            [false, 400],
+            ['', 404],
+        ]);
+    });
+
+    it('should validate number minimums and maximums', async () => {
+        await testApiMethodInput('/numberMinMax/:num', 'num', [
+            [5, 200],
+            [6, 200],
+            [10, 200],
+            [1, 400],
+            [11, 400],
+            ['a', 400],
+            [true, 400],
+            [false, 400],
+            ['', 404],
+        ]);
+    });
+
+    it('should validate string regexes', async () => {
+        await testApiMethodInput('/stringRegex/:str', 'str', [
+            ['a', 200],
+            [5, 400],
+            ['', 404],
+            ['6', 400],
+            ['I', 400],
+            ['i', 200],
+        ]);
+    });
+
+    it('should call validation functions', async () => {
+        await testApiMethodInput('/validationFunc/:str', 'str', [
+            ['valid', 200],
+            ['a', 400],
+            [5, 400],
+            ['', 404],
+            [true, 400],
+            [false, 400],
+            ['6', 400],
+            ['I', 400],
+            ['i', 400],
+            ['valid', 200],
+        ]);
+    });
+});
---
+++ packages/ts-api-decorators/test/parameter-runtime-tests/sources/path-params.ts
@@ -0,0 +1,50 @@
+import { Api, ApiGetMethod, ApiBodyParam, ApiPathParamNumber, ApiPathParamString, ApiPathParam, HttpBadRequestError } from "../../../src";
+import { ApiInjectedDependencyParam, ApiInjectedDependency } from "../../../src/decorators/DependencyParams";
+import { TestManagedApi } from "../../TestTransport";
+
+@Api
+class MyApi {
+	@ApiGetMethod('/numberMin/:num')
+	numberMin(
+		@ApiPathParamNumber(undefined, 0) num: number
+	) {
+		return num.toString()
+	}
+
+	@ApiGetMethod('/numberMax/:num')
+	numberMax(
+		@ApiPathParamNumber(undefined, undefined, 0) num: number
+	) {
+		return num.toString()
+	}
+
+	@ApiGetMethod('/numberMinMax/:num')
+	numberMinMax(
+		@ApiPathParamNumber(undefined, 5, 10) num: number
+	) {
+		return num.toString()
+	}
+
+	@ApiGetMethod('/stringRegex/:str')
+	string(
+		@ApiPathParamString(undefined, /^[a-z]+$/) str: string
+	) {
+		return str;
+	}
+
+	@ApiGetMethod('/validationFunc/:str')
+	validationFunc(
+		@ApiPathParam(undefined, (_, i) => {
+			if (i !== 'valid') {
+				throw new HttpBadRequestError('Expected str to be valid');
+			}
+		}) str: string
+	) {
+		return str;
+	}
+
+}
+
+const testApi = new TestManagedApi();
+testApi.addHandlerClass(MyApi);
+export default testApi;
