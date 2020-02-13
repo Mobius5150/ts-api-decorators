a6a4a9dd5f91b121167b1fa2318a11604d61662b
Mobius5150 <mike@michaelblouin.ca>
2020-02-12 21:00:01 -0800

    v0.1.5

---
+++ packages/ts-api-decorators/test/parameter-runtime-tests/sources/header-params.ts
@@ -0,0 +1,49 @@
+import { Api, ApiGetMethod, ApiHeaderParamNumber, ApiHeaderParamString, ApiHeaderParam, HttpBadRequestError } from "../../../src";
+import { TestManagedApi } from "../../TestTransport";
+
+@Api
+class MyApi {
+	@ApiGetMethod('/numberMin')
+	numberMin(
+		@ApiHeaderParamNumber(undefined, 0) num: number
+	) {
+		return num.toString()
+	}
+
+	@ApiGetMethod('/numberMax')
+	numberMax(
+		@ApiHeaderParamNumber(undefined, undefined, 0) num: number
+	) {
+		return num.toString()
+	}
+
+	@ApiGetMethod('/numberMinMax')
+	numberMinMax(
+		@ApiHeaderParamNumber(undefined, 5, 10) num: number
+	) {
+		return num.toString()
+	}
+
+	@ApiGetMethod('/stringRegex')
+	string(
+		@ApiHeaderParamString(undefined, /^[a-z]+$/) str: string
+	) {
+		return str;
+	}
+
+	@ApiGetMethod('/validationFunc')
+	validationFunc(
+		@ApiHeaderParam(undefined, (_, i) => {
+			if (i !== 'valid') {
+				throw new HttpBadRequestError('Expected str to be valid');
+			}
+		}) str: string
+	) {
+		return str;
+	}
+
+}
+
+const testApi = new TestManagedApi();
+testApi.addHandlerClass(MyApi);
+export default testApi;
